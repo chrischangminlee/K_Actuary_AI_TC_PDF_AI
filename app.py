5a09d865b48f3d1df67f20bb2a4a5fa6c8e13363
brokenankle94 <chrislee9407@gmail.com>
2025-09-18 07:32:19 +0900

    update to 2.0

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.sidebar.markdown("""
 본 AI 챗 서비스는 한국 계리업무를 수행하는 계리사를 위해 개발된 개인 프로젝트로 만들어진 AI Chatbot / Agent입니다.
                     
 본 pdf 분석 AI Agent와 더불어 현재 다양한 유용한 기능들이 지속적으로 개발 중이며, 보다 향상된 서비스를 제공하기 위해 개선을 이어가고 있습니다.
-* Gemini 1.5 flash model을 사용하여 PDF를 분석하고 있어 답변 생성 속도가 느립니다.
+* Gemini 2.0 flash model을 사용하여 PDF를 분석하고 있어 답변 생성 속도가 느립니다.
 """)
 st.sidebar.markdown('<p style="color: red; font-size: 0.8em;">(주의) 본 AI가 제공하는 답변은 참고용이며, 정확성을 보장할 수 없습니다. 보안을 위해 회사 기밀, 개인정보등은 제공하지 않기를 권장드리며, 반드시 실제 업무에 적용하기 전에 검토하시길 바랍니다.</p>', unsafe_allow_html=True)
 
@@ -256,7 +256,7 @@ if st.button("전송"):
         st.info(f"PDF 페이지 수: {total_pages}페이지")
         
         # 모델 초기화
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-2.0-flash')
         
         # 시스템 프롬프트
         system_prompt = (
